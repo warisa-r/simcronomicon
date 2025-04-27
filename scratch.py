5a7377d89cf26b4d5cc151e07a30b397a3fc6744
Warisa <compund555@gmail.com>
2025-04-27 15:57:38 +0200

    fix social energy bug

--- scratch.py
+++ scratch.py
@@ -1,8 +1,8 @@
 import simcronomicon as scon
 
-town_graph = scon.create_town_graph(600, 0.7)
-town = scon.Town(town_graph, 0.4, 200)
-sim_params = scon.SimulationParameters(0.3, 0.5, 0.5, 0.4, 0.7, 0.2, 0.001, 0.001)
+town_graph = scon.create_town_graph(5000, 0.5)
+town = scon.Town(town_graph, 0.5, 200)
+sim_params = scon.SimulationParameters(0.7, 0.5, 0.5, 0.4, 0.7, 0.2, 0.1, 0.001)
 print(sim_params)
 sim = scon.Simulation(town, sim_params, 100)
 sim.run()
--- simcronomicon/folk.py
+++ simcronomicon/folk.py
@@ -46,7 +46,7 @@ class Folk:
                 self.convert('S', 'R', status_dict_t)
             else:
                 self.spreader_streak += 1
-        self.social_energy = rd.randint(4, 10) # Reset social energy
+        self.social_energy = rd.randint(0, 2) # Reset social energy
 
     def __repr__(self):
         return f"Person live at {self.home_address}, currently at {self.address}, Social Energy={self.social_energy}, Status={self.status}"
