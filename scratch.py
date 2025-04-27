1063efbfb80a84c58fc2f34484b8aa6c29aa0fce
Warisa <compund555@gmail.com>
2025-04-27 14:47:47 +0200

    change of parameters

--- scratch.py
+++ scratch.py
@@ -1,8 +1,9 @@
 import simcronomicon as scon
 
-town_graph = scon.create_town_graph(600, 0.1)
-town = scon.Town(town_graph, 0.4, 400)
-sim_params = scon.SimulationParameters(0.04, 0.05, 0.05, 0.04, 0.07, 0.05, 0.001, 0.001)
+town_graph = scon.create_town_graph(600, 0.7)
+town = scon.Town(town_graph, 0.4, 200)
+sim_params = scon.SimulationParameters(0.3, 0.5, 0.5, 0.4, 0.7, 0.2, 0.001, 0.001)
+print(sim_params)
 sim = scon.Simulation(town, sim_params, 100)
 sim.run()
 print(sim.status_dicts[-1])
